7c43c8543d3df1a178191101bf943a2c6945731b
Travy256 <hkizito.std@nkumbauniversity.ac.ug>
2023-10-29 08:47:15 -0700

    adding flask

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask("HANK ONLINE HARDWARE",template_folder="html",static_folder="static")
+
+app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+@app.route('/')
+def home():
+    return render_template("Index.html")
+
+app.run()    
+
