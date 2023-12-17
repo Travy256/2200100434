5cb86e2959e6139423def033f61539d2a3b6a7c7
Jacob Philpott <jacob.philpott@gmx.com>
2023-12-17 17:55:30 +0300

    adding products pages

--- index.py
+++ index.py
@@ -8,12 +8,28 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 def home():
     return render_template("Index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/products')
+def products():
+    return render_template("products/index.html")
+
+@app.route('/products/paint')
+def products_paint():
+    return render_template("products/paint.html")
+
+@app.route('/products/roofing')
+def products_roofing():
+    return render_template("products/roofing.html")
+
+@app.route('/products/cement')
+def products_cement():
+    return render_template("products/cement.html")
 
 @app.route('/contacts')
 def contacts():
     return render_template("contacts.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 app.run(host="0.0.0.0", port=4000)
